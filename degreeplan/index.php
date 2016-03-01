be061a40017fec3850d61611abe419e59bb3574a
Andrew Hackmann <ajhack93@gmail.com>
2016-03-01 15:56:29 -0600

    Update index.php
    Took out the direct links to the church website.

--- degreeplan/index.php
+++ degreeplan/index.php
@@ -6,13 +6,13 @@
 
 <!-- REDIRECTING STARTS -->
 
-<link rel="canonical" href="http://tribulationperiod.com/degreeplan/cs.php"/>
+<link rel="canonical" href="degreeplan/cs.php"/>
 <noscript>
-	<meta http-equiv="refresh" content="0;URL=http://tribulationperiod.com/degreeplan/cs.php">
+	<meta http-equiv="refresh" content="0;URL=degreeplan/cs.php">
 </noscript>
 <!--[if lt IE 9]><script type="text/javascript">var IE_fix=true;</script><![endif]-->
 <script type="text/javascript">
-	var url = "http://tribulationperiod.com/degreeplan/cs.php";
+	var url = "degreeplan/cs.php";
 	if(typeof IE_fix != "undefined") // IE8 and lower fix to pass the http referer
 	{
 		document.write("redirecting..."); // Don't remove this line or appendChild() will fail because it is called before document.onload to make the redirect as fast as possible. Nobody will see this text, it is only a tech fix.
