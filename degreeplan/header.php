f660737c4aa0988d92d0ac5877570f96a49fbcff
Andrew Hackmann <ahackmann1@atu.edu>
2016-03-08 15:21:53 -0600

    Changed the classes to a dropdown file

--- degreeplan/header.php
+++ degreeplan/header.php
@@ -1,15 +1,32 @@
 
+<?php
+	$submittedValue = "";
+	$value0 = "";
+	$a = array ('cs', 'is', 'it');
+	if (isset($_POST["FruitList"])) {
+		$submittedValue = $_POST["FruitList"];
+	}
+	$arrlength = count($a);
+?>
 <img src="ATU.png" alt="ATU" style="width:470px;height:97px;">
 <h2>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspDegree Planner</h2><style>
 <!-- This is where the links to the top degree menus is located at-->
-</style><div id="menu">
-<ul>
-  <li><a href="cs.php">CS</a></li>
-  <li><a href="is.php">IS</a></li>
-  <li><a href="it.php">IT</a></li>
+</style>
+<form name="DropDown">
+<div id="menu">
+<select onChange="location=this.options[this.selectedIndex].value;">
+	<option> </option>
+  	<?php for($x = 0; $x < $arrlength; $x++){
+		echo "<option value = \"";
+		echo $a[$x];
+		echo "\">";
+		echo $a[$x];
+		echo "</option>";
+		} ?>
+</select>
+
 
   <li><a id ="Instructions" href="#instructionsModal" data-toggle="modal">Help</a></li> 
   <a href="#wipePlanModal" data-toggle="modal">Reset plan to default</a>
-		 <li>
-
-
+</div>
+</form>
